fba4d3c1aa658311af25b4cd966934d6c5e80faf
Jean Paul Molfino <jeanpaulmolfino@gmail.com>
2020-11-14 02:32:51 -0300

    Respaldo de base de datos

---
+++ scada.sql
@@ -0,0 +1,165 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 14-11-2020 a las 06:32:00
+-- Versión del servidor: 10.4.14-MariaDB
+-- Versión de PHP: 7.4.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `scada`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `dispositivos`
+--
+
+CREATE TABLE `dispositivos` (
+  `id` int(11) NOT NULL,
+  `actividad` tinyint(1) NOT NULL,
+  `ubicacion` varchar(60) NOT NULL,
+  `fecha_alta` date NOT NULL,
+  `fecha_baja` date DEFAULT NULL,
+  `id_tipo` int(11) NOT NULL,
+  `marca` varchar(40) NOT NULL,
+  `modelo` varchar(40) NOT NULL,
+  `ultima_actualizacion` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `logdispositivos`
+--
+
+CREATE TABLE `logdispositivos` (
+  `id` int(11) NOT NULL,
+  `id_dispositivo` int(11) NOT NULL,
+  `descripcion` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `medidas`
+--
+
+CREATE TABLE `medidas` (
+  `id` int(11) NOT NULL,
+  `tiempo` datetime NOT NULL,
+  `lectura` varchar(10) NOT NULL,
+  `id_dispositivo` int(11) NOT NULL,
+  `id_sensor` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipos_dispositivos`
+--
+
+CREATE TABLE `tipos_dispositivos` (
+  `id` int(11) NOT NULL,
+  `updatetime` int(20) NOT NULL,
+  `categoria` varchar(40) NOT NULL,
+  `proposito` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `dispositivos`
+--
+ALTER TABLE `dispositivos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_dispositivo_tipos` (`id_tipo`);
+
+--
+-- Indices de la tabla `logdispositivos`
+--
+ALTER TABLE `logdispositivos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_logdispositivos_dispositivos` (`id_dispositivo`);
+
+--
+-- Indices de la tabla `medidas`
+--
+ALTER TABLE `medidas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_medidas_dispositivos` (`id_dispositivo`);
+
+--
+-- Indices de la tabla `tipos_dispositivos`
+--
+ALTER TABLE `tipos_dispositivos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `dispositivos`
+--
+ALTER TABLE `dispositivos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `logdispositivos`
+--
+ALTER TABLE `logdispositivos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `medidas`
+--
+ALTER TABLE `medidas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tipos_dispositivos`
+--
+ALTER TABLE `tipos_dispositivos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `dispositivos`
+--
+ALTER TABLE `dispositivos`
+  ADD CONSTRAINT `fk_dispositivo_tipos` FOREIGN KEY (`id_tipo`) REFERENCES `tipos_dispositivos` (`id`);
+
+--
+-- Filtros para la tabla `logdispositivos`
+--
+ALTER TABLE `logdispositivos`
+  ADD CONSTRAINT `fk_logdispositivos_dispositivos` FOREIGN KEY (`id_dispositivo`) REFERENCES `dispositivos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `medidas`
+--
+ALTER TABLE `medidas`
+  ADD CONSTRAINT `fk_medidas_dispositivos` FOREIGN KEY (`id_dispositivo`) REFERENCES `dispositivos` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
