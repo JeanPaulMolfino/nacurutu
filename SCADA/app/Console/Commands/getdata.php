8a9fef718a02c8688431576534e02139123fd4e3
Jean Paul Molfino <jeanpaulmolfino@gmail.com>
2020-12-03 01:13:30 -0300

    Arreglos al comando
    Funciona basicamente

--- SCADA/app/Console/Commands/getdata.php
+++ SCADA/app/Console/Commands/getdata.php
@@ -40,22 +40,30 @@ class getdata extends Command
     {
         //Con esto pretendo que me traiga todos los id de dispositivos que tengan tareas pendientes en conjunto a su ultima medicion
         // o aquellos que ya halla excedido el tiempo de actualizacion desde que se les hizo update
-        $dispositivos = DB::select('select d.id, d.ultima_actualizacion from dispositivos as d inner join tipos_dispositivos as t where d.id_tipo = t.id and d.actividad = 1 and (DATE_ADD(d.ultima_actualizacion, INTERVAL t.updatetime MINUTE) < NOW())');
-
+        $dispositivos = DB::select("select d.id, d.identificador, DATE_FORMAT(d.ultima_actualizacion,'%Y-%m-%d-%H-%i-%S') as ultima_actualizacion from dispositivos as d inner join tipos_dispositivos as t where d.id_tipo = t.id and d.actividad = 1 and (DATE_ADD(d.ultima_actualizacion, INTERVAL t.updatetime MINUTE) < NOW())");
+        var_dump($dispositivos);
         //itero sobre cada uno de los dispositivos
         foreach ($dispositivos as $dispositivo) {
-
+            //Cambio el formato de la fecha
+            //$fecha = $dispositivo->ultima_actualizacion;
+            //$fecha[10] = "-";
+            //$fecha[13] = "-";
+            //$fecha[16] = "-";
+            var_dump($dispositivo);
             //Obtengo la informacion nueva del $dispositivo
-            $data = file_get_contents('http://alguna');
+            $data = file_get_contents('http://127.0.0.1:8000/dummyDevice?ID_DEV='.$dispositivo->identificador.'&MODE=SINCE&T='.$dispositivo->ultima_actualizacion);
 
             //Si no es vacio
             if ($data != '') {
                 $separator = "\r\n"; //Suponiendo que sea este el separador entre mediciones
                 $lineas = explode($separator, $data); //lo divido en lineas o de cada lectura
-
+                array_shift($lineas);
                 //Aquí voy a guardar la ultima fecha de los registros
                 $ultimaactualizacion = "";
-
+                if(end($lineas) == ""){
+                    array_pop($lineas);
+                }
+                //var_dump($lineas);
                 //Por cada linea
                 foreach($lineas as $linea){
 
@@ -66,18 +74,22 @@ class getdata extends Command
                     $cantidad = count($datos);
 
                     //Y por cada dato
-                    $index = 0;
+                    $index = 1;
                     while($index < $cantidad){
                         //Lo inserto en la base de datos
-                        DB::insert('insert into medidas (tiempo, lectura, id_dispositivo, id_sensor) values (?, ?, ?, ?)', [$datos[0], $datos[$index+1], $dispositivo[0], $index+1]);
+                        DB::insert('insert into medidas (tiempo, lectura, id_dispositivo, id_sensor) values (?, ?, ?, ?)', [$datos[0], $datos[$index], $dispositivo->id, $index]);
                         $index++;
                     }
                     //Voy sobre escribiendo la ultima actualizacion
                     $ultimaactualizacion = $datos[0];
                 }
-
+                //Cambio el formato a la fecha
+                /*$ultimaactualizacion[10] = "-";
+                $ultimaactualizacion[13] = "-";
+                $ultimaactualizacion[16] = "-";
+                var_dump($ultimaactualizacion);*/
                 //Una ves que termino de insertar todas, actualizo la fecha de su ultima lectura
-                DB::update('update dispositivos set ultima_actualizacion = ? where id = ?', [$ultimaactualizacion, $dispositivo[0]]);
+                DB::update('update dispositivos set ultima_actualizacion = ? where id = ?', [$ultimaactualizacion, $dispositivo->id]);
                 
             }
         }
