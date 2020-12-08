e22c10fc77714293652259acb18ddbcdd79a2d12
Jean Paul Molfino <jeanpaulmolfino@gmail.com>
2020-12-07 22:36:45 -0300

    Update codificacion endpoints

--- SCADA/app/Http/Controllers/ctrlDispositivos.php
+++ SCADA/app/Http/Controllers/ctrlDispositivos.php
@@ -2,40 +2,39 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class ctrlDispositivos extends Controller
 {
     public function get_dispositivobyid($id)
     {
-		$data = json_encode(DB::select("Select d.id, d.actividad, d.ubicacion, td.categoria, d.ultima_actualizacion from dispositivos as d inner join tipos_dispositivos as td where td.id = d.id_tipo and d.id=?", [$id]));
-		return response($data, 200) ->header('Content-Type', 'application/json');
+        $data = json_encode(DB::select("Select d.id, d.actividad, d.ubicacion, td.categoria, d.ultima_actualizacion from dispositivos as d inner join tipos_dispositivos as td where td.id = d.id_tipo and d.id=?", [$id]));
+        return response($data, 200)->header('Content-Type', 'application/json');
     }
 
     public function get_dispositivobyidentificador($identificador)
     {
-		$data = json_encode(DB::select("Select d.id, d.actividad, d.ubicacion, td.categoria, d.ultima_actualizacion from dispositivos as d inner join tipos_dispositivos as td where td.id = d.id_tipo and d.identificador=?", [$identificador]));
-		return response($data, 200) ->header('Content-Type', 'application/json');
+        $data = json_encode(DB::select("Select d.id, d.actividad, d.ubicacion, td.categoria, d.ultima_actualizacion from dispositivos as d inner join tipos_dispositivos as td where td.id = d.id_tipo and d.identificador=?", [$identificador]));
+        return response($data, 200)->header('Content-Type', 'application/json');
     }
-    
+
     public function insert_dispositivo($ubicacion, $fecha_alta, $id_tipo, $marca, $modelo, $identificador)
     {
-		DB::insert("insert into dispositivos (actividad, ubicacion, fecha_alta, id_tipo, marca, modelo, ultima_actualizacion, identificador) values ('1', ?, ?, ?, ?, ?, '1900-01-01 00:00:00', ?)", [$ubicacion, $fecha_alta, $id_tipo, $marca, $modelo, $identificador]);
+        DB::insert("insert into dispositivos (actividad, ubicacion, fecha_alta, id_tipo, marca, modelo, ultima_actualizacion, identificador) values ('1', ?, ?, ?, ?, ?, '1900-01-01 00:00:00', ?)", [$ubicacion, $fecha_alta, $id_tipo, $marca, $modelo, $identificador]);
     }
-    
+
     public function update_dispositivo($id, $ubicacion, $id_tipo, $marca, $modelo, $identificador)
     {
-		DB::update("update dispositivos set ubicacion = ?, id_tipo = ?, marca = ?, modelo = ?, identificador = ? where id = ?", [$ubicacion, $id_tipo, $marca, $modelo, $identificador, $id]);
+        DB::update("update dispositivos set ubicacion = ?, id_tipo = ?, marca = ?, modelo = ?, identificador = ? where id = ?", [$ubicacion, $id_tipo, $marca, $modelo, $identificador, $id]);
     }
-    
+
     public function baja_dispositivo($identificador)
     {
-		DB::update("update dispositivos set actividad = '0' where identificador = ?", [$identificador]);
+        DB::update("update dispositivos set actividad = '0' where identificador = ?", [$identificador]);
     }
-    
+
     public function alta_dispositivo($identificador)
     {
-		DB::update("update dispositivos set actividad = '1' where identificador = ?", [$identificador]);
-	}
+        DB::update("update dispositivos set actividad = '1' where identificador = ?", [$identificador]);
+    }
 }
--- SCADA/app/Http/Controllers/endpoints.php
+++ SCADA/app/Http/Controllers/endpoints.php
@@ -9,48 +9,52 @@ class endpoints extends Controller
 {
     public function get_dispositivos()
     {
-		$data = json_encode(DB::select("Select d.id, d.identificador, d.actividad, d.ubicacion, td.categoria, d.ultima_actualizacion from dispositivos as d inner join tipos_dispositivos as td where td.id = d.id_tipo"));
-		return response($data, 200) ->header('Content-Type', 'application/json');
-	}
-	
-	public function get_dispositivos_tipos()
-    {
-		$data = json_encode(DB::select("Select td.* from tipos_dispositivos as td"));
-        return response($data, 200) ->header('Content-Type', 'application/json');
+        $data = json_encode(DB::select("Select d.id, d.identificador, d.actividad, d.ubicacion, td.categoria, d.ultima_actualizacion from dispositivos as d inner join tipos_dispositivos as td where td.id = d.id_tipo"));
+        return response($data, 200)->header('Content-Type', 'application/json');
     }
 
-	public function get_sensores()
+    public function get_dispositivos_tipos()
     {
-		$data = json_encode(DB::select("Select s.* from sensores as s"));
-		return response($data, 200) ->header('Content-Type', 'application/json');
-	}
+        $data = json_encode(DB::select("Select td.* from tipos_dispositivos as td"));
+        return response($data, 200)->header('Content-Type', 'application/json');
+    }
 
-	public function get_sensores_tipos()
+    public function get_sensores()
     {
-		$data = json_encode(DB::select("Select st.* from sensores_tipos as st"));
-        return response($data, 200) ->header('Content-Type', 'application/json');
-	}
-	
+        $data = json_encode(DB::select("Select s.* from sensores as s"));
+        return response($data, 200)->header('Content-Type', 'application/json');
+    }
+
+    public function get_sensores_tipos()
+    {
+        $data = json_encode(DB::select("Select st.* from sensores_tipos as st"));
+        return response($data, 200)->header('Content-Type', 'application/json');
+    }
+
     public function get_sensoresbydispositivo($id)
     {
-        return json_encode(DB::select("Select s.* from dispositivos as d inner join tipos_dispositivos as td inner join sensores as s where td.id = d.id_tipo and td.id = s.id_tipo and d.identificador='".$id."'"), JSON_UNESCAPED_UNICODE);
+        $data = json_encode(DB::select("Select s.* from dispositivos as d inner join tipos_dispositivos as td inner join sensores as s where td.id = d.id_tipo and td.id = s.id_tipo and d.identificador='" . $id . "'"), JSON_UNESCAPED_UNICODE);
+        return response($data, 200)->header('Content-Type', 'application/json');
     }
 
-    public function get_medidasbydispositivo(Request $request, $iddispositivo, $idsensor, $finicio, $ffin){
-        if($finicio != 'null' && $ffin != 'null'){
+    public function get_medidasbydispositivo(Request $request, $iddispositivo, $idsensor, $finicio, $ffin)
+    {
+        if ($finicio != 'null' && $ffin != 'null') {
             //$finicio[10] = ' ';
             //$ffin[10] = ' ';
-            return json_encode(DB::select("select m.lectura, m.tiempo from medidas as m inner join dispositivos as d where m.id_dispositivo=d.id and m.tiempo >='".$finicio."' and m.tiempo <= '".$ffin."' and m.id_sensor=".$idsensor." and d.identificador='".$iddispositivo."'"), JSON_UNESCAPED_UNICODE);
-        }
-        else if($finicio != 'null' && $ffin === 'null'){
+            $data = json_encode(DB::select("select m.lectura, m.tiempo from medidas as m inner join dispositivos as d where m.id_dispositivo=d.id and m.tiempo >='" . $finicio . "' and m.tiempo <= '" . $ffin . "' and m.id_sensor=" . $idsensor . " and d.identificador='" . $iddispositivo . "'"), JSON_UNESCAPED_UNICODE);
+            return response($data, 200)->header('Content-Type', 'application/json');
+        } else if ($finicio != 'null' && $ffin === 'null') {
             //$finicio[10] = ' ';
-            return json_encode(DB::select("select m.lectura, m.tiempo from medidas as m inner join dispositivos as d where m.id_dispositivo=d.id and m.tiempo >='".$finicio."' and m.id_sensor=".$idsensor." and d.identificador='".$iddispositivo."'"), JSON_UNESCAPED_UNICODE);
-        }
-        else if($finicio === 'null' && $ffin != 'null'){
+            $data = json_encode(DB::select("select m.lectura, m.tiempo from medidas as m inner join dispositivos as d where m.id_dispositivo=d.id and m.tiempo >='" . $finicio . "' and m.id_sensor=" . $idsensor . " and d.identificador='" . $iddispositivo . "'"), JSON_UNESCAPED_UNICODE);
+            return response($data, 200)->header('Content-Type', 'application/json');
+        } else if ($finicio === 'null' && $ffin != 'null') {
             //$ffin[10] = ' ';
-            return json_encode(DB::select("select m.lectura, m.tiempo from medidas as m inner join dispositivos as d where m.id_dispositivo=d.id and m.tiempo <='".$ffin."' and m.id_sensor=".$idsensor." and d.identificador='".$iddispositivo."'"), JSON_UNESCAPED_UNICODE);
+            $data = json_encode(DB::select("select m.lectura, m.tiempo from medidas as m inner join dispositivos as d where m.id_dispositivo=d.id and m.tiempo <='" . $ffin . "' and m.id_sensor=" . $idsensor . " and d.identificador='" . $iddispositivo . "'"), JSON_UNESCAPED_UNICODE);
+            return response($data, 200)->header('Content-Type', 'application/json');
         } else {
-            return json_encode(DB::select("select m.lectura, m.tiempo from medidas as m inner join dispositivos as d where m.id_dispositivo=d.id and m.id_sensor=".$idsensor." and d.identificador='".$iddispositivo."'"), JSON_UNESCAPED_UNICODE);
+            $data = json_encode(DB::select("select m.lectura, m.tiempo from medidas as m inner join dispositivos as d where m.id_dispositivo=d.id and m.id_sensor=" . $idsensor . " and d.identificador='" . $iddispositivo . "'"), JSON_UNESCAPED_UNICODE);
+            return response($data, 200)->header('Content-Type', 'application/json');
         }
         //return json_decode(DB::select("Select "));
     }
