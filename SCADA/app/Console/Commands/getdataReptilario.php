a46f5ce75a2581820e98e23a3b876447430fe430
Jean Paul Molfino <jeanpaulmolfino@gmail.com>
2020-12-21 20:47:53 -0300

    Hotfix dummies

--- SCADA/app/Console/Commands/getdataReptilario.php
+++ SCADA/app/Console/Commands/getdataReptilario.php
@@ -42,12 +42,12 @@ class getdataReptilario extends Command
         // o aquellos que ya halla excedido el tiempo de actualizacion desde que se les hizo update
         $id_tipo = DB::select("select * from tipos_dispositivos where categoria='Reptilario'");
         $dispositivos = DB::select("select d.id, d.identificador, d.id_tipo,DATE_FORMAT(d.ultima_actualizacion,'%Y-%m-%d-%H-%i-%S') as ultima_actualizacion from dispositivos as d inner join tipos_dispositivos as t where d.id_tipo = t.id and d.actividad = 1 and (DATE_ADD(d.ultima_actualizacion, INTERVAL t.updatetime MINUTE) < NOW() and t.categoria = 'Reptilario')");
-        $sensores = DB::select("select * from sensores as s inner join where id_tipo = ? and nombre = '?'", [$id_tipo, "Humedad del ambiente"]);
+        $sensores = DB::select("select * from sensores as s where s.id_tipo = ? and s.nombre = '?'", [$id_tipo, "Humedad del ambiente"]);
         foreach ($dispositivos as $dispositivo) {
             var_dump($dispositivo);
             //Humedad del ambiente 65 a 75%
             $fecha = date("Y/m/d H:i:s");
-            DB::insert('insert into medidas (tiempo, lectura, id_dispositivo, id_sensor) values (?, ?, ?, ?)', [$fecha, rand (65,75), $dispositivo->id, $sensores[0]->id]);
+            DB::insert('insert into medidas (tiempo, lectura, id_dispositivo, id_sensor) values (?, ?, ?, ?)', [$fecha, (strval(rand (65,75))), $dispositivo->id, $sensores[0]->id]);
             DB::update('update dispositivos set ultima_actualizacion = ? where id = ?', [$fecha, $dispositivo->id]);
         }
         return 0;
